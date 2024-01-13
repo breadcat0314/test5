7b99a5352064e2b125a4986e85ea84f9643d50b6
Anthony Carbone <85899007+breadcat0314@users.noreply.github.com>
2024-01-13 16:22:31 -0500

    Update sentry.ts

--- plugins/template/src/lib/sentry.ts
+++ plugins/template/src/lib/sentry.ts
@@ -1,14 +1,14 @@
-import { findByProps } from "@vendetta/metro";
-import { noop } from "./utils";
+//import { findByProps } from "@vendetta/metro";
+//import { noop } from "./utils";
 
-const Sentry = {
+//const Sentry = {
     initializer: findByProps("initSentry"),
     main: globalThis.__SENTRY__?.hub,
     client: globalThis.__SENTRY__?.hub?.getClient(),
 };
 
-export default () => {
-    const patches = [
+//export default () => {
+    //const patches = [
         noop("initSentry", Sentry.initializer),
         noop("addBreadcrumb", Sentry.main),
     ];
@@ -18,7 +18,7 @@ export default () => {
     Sentry.main.getStackTop().scope.clear();
     Sentry.main.getScope().clear();
 
-    return () => {
+    //return () => {
         patches.forEach((p) => p());
         Sentry.client.getOptions().enabled = true;
         Sentry.client.open();
